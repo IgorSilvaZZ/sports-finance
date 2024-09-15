a6a4c577dd1083c015872e20546b1203e17caf6a
IgorSilvaZZ <igor06silva12@gmail.com>
2024-09-15 18:40:25 -0300

    feat: :sparkles: Initial Project

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+};
