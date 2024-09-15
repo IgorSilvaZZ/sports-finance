595aab79e99b96b2e8a2017da8fed7bc5c8725c7
IgorSilvaZZ <igor06silva12@gmail.com>
2024-09-15 19:29:58 -0300

    perf: :construction: Add main modules

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
+import { ParticipantModule } from './participant/participant.module';
+import { CategoryModule } from './category/category.module';
+import { EventModule } from './event/event.module';
+import { HistoryModule } from './history/history.module';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    ParticipantModule,
+    CategoryModule,
+    EventModule,
+    HistoryModule,
+    PaymentModule,
+  ],
   controllers: [],
   providers: [],
 })
