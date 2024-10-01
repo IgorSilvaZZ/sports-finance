3063dd309ba04abfa22a2a94612c07ea860a78ce
IgorSilvaZZ <igor06silva12@gmail.com>
2024-09-30 22:44:27 -0300

    feat: :pencil2: Add list event by id

--- server/src/event/event.controller.ts
+++ server/src/event/event.controller.ts
@@ -1,15 +1,25 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 import { CreateEventDTO } from './dtos/CreateEventDTO';
+
 import { CreateEventUseCase } from './useCases/CreateEventUseCase';
 import { ListEventsByResponsibleIdUseCase } from './useCases/ListEventsByResponsibleIdUseCase';
+import { ListEventByIdUseCase } from './useCases/ListEventByIdUseCase';
 
 @Controller('/events')
 export class EventController {
   constructor(
     private createEventUseCase: CreateEventUseCase,
     private listEventsByResponsibleIdUseCase: ListEventsByResponsibleIdUseCase,
+    private listEventByIdUseCase: ListEventByIdUseCase,
   ) {}
+
+  @Get('/:eventId')
+  async getById(@Param('eventId') eventId: string) {
+    const event = await this.listEventByIdUseCase.execute(eventId);
+
+    return event;
+  }
 
   @Get('/responsible/:responsibleId')
   async getEventsByResponsible(@Param('responsibleId') responsibleId: string) {
---
+++ server/src/event/useCases/ListEventByIdUseCase.spec.ts
@@ -0,0 +1,49 @@
+import { NotFoundException } from '@nestjs/common';
+import { faker } from '@faker-js/faker/.';
+
+import { EventRepositoryInMemory } from '../../../test/repositories/EventRepositoryInMemory';
+import { ResponsibleRepositoryInMemory } from '../../../test/repositories/ResponsibleRepositoryInMemory';
+
+import { ListEventByIdUseCase } from './ListEventByIdUseCase';
+
+let eventRepositoryInMemory: EventRepositoryInMemory;
+let responsibleRepositoryInMemory: ResponsibleRepositoryInMemory;
+let listEventByIdUseCase: ListEventByIdUseCase;
+
+describe('List event by id', () => {
+  beforeEach(() => {
+    eventRepositoryInMemory = new EventRepositoryInMemory();
+    responsibleRepositoryInMemory = new ResponsibleRepositoryInMemory();
+    listEventByIdUseCase = new ListEventByIdUseCase(eventRepositoryInMemory);
+  });
+
+  it('should be able list event by id', async () => {
+    const nameEvent = 'Event Test';
+
+    const responsible = await responsibleRepositoryInMemory.create({
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      phoneNumber: faker.phone.number(),
+      password: '123',
+    });
+
+    const event = await eventRepositoryInMemory.create({
+      name: nameEvent,
+      type: 'Futebol',
+      description: 'Event test created',
+      responsibleId: responsible.id,
+    });
+
+    const events = await listEventByIdUseCase.execute(event.id);
+
+    expect(event).toBeTruthy();
+    expect(event.id).toEqual(event.id);
+    expect(events.name).toEqual(nameEvent);
+  });
+
+  it('should not be able list event by id not found', () => {
+    expect(async () => {
+      return listEventByIdUseCase.execute('event-id-not-found');
+    }).rejects.toEqual(new NotFoundException('Event not found!'));
+  });
+});
