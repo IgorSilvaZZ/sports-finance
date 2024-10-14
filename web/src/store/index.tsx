7f08424354ecfac0c9451f4882c39c8db08e474e
IgorSilvaZZ <igor06silva12@gmail.com>
2024-10-14 20:26:32 -0300

    feat: :heavy_plus_sign: Add event state in redux

--- web/src/store/index.tsx
+++ web/src/store/index.tsx
@@ -3,6 +3,7 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 
 import responsibleSlice from "./responsible/responsible.slice";
+import eventSlice from "./events/event.slice";
 
 const persistConfig = {
   key: "my-finances",
@@ -11,6 +12,7 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   responsible: responsibleSlice,
+  event: eventSlice,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
