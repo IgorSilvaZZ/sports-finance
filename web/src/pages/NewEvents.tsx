ba2231f2d163b63fd70e48cb1f5b5800d538bd42
IgorSilvaZZ <igor06silva12@gmail.com>
2025-04-17 23:10:56 -0300

    refactor: :pencil2: Add integration in new card event

--- web/src/pages/NewEvents.tsx
+++ web/src/pages/NewEvents.tsx
@@ -1,24 +1,78 @@
-// #e5eaf6 => Cor de fundo
-
-import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { ClipLoader } from "react-spinners";
 
 import { ArrowLeft } from "@phosphor-icons/react";
+
+import { Event } from "../interfaces/Event.interface";
 
 import { NewCardsEvent } from "../components/NewCardsEvent";
 
 import { dashboardActions } from "../store/dashboard/dashboard.slice";
-import { responsibleActions } from "../store/responsible/responsible.slice";
+import {
+  responsibleActions,
+  selectResponsible,
+} from "../store/responsible/responsible.slice";
+
+import { deleteEventById, getEventsByResponsible } from "../services/event";
+
+import emptyImage from "../assets/list-event-empty.png";
+import { ModalCreateEvent } from "../components/ModalCreateEvent";
 
 export default function NewEvents() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const responsible = useSelector(selectResponsible);
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [events, setEvents] = useState<Event[]>([]);
 
   function goToBack() {
     dispatch(dashboardActions.clearFilters());
     dispatch(responsibleActions.clear());
     navigate("/");
   }
+
+  function onViewDetails(eventId: string) {
+    navigate(`/event/${eventId}`);
+  }
+
+  async function getEvents() {
+    setIsLoading(true);
+
+    try {
+      const events = await getEventsByResponsible(responsible.id);
+
+      setEvents(events);
+    } catch (error) {
+      console.log(error);
+
+      toast.error("Ocorreu algum erro ao listar os eventos!");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function onDeleteEvent(eventId: string) {
+    try {
+      await deleteEventById(eventId, responsible.id);
+
+      toast.success("Evento deletado com sucesso!");
+
+      getEvents();
+    } catch (error) {
+      console.log(error);
+
+      toast.error("Ocorreu algum tentar excluir o evento!");
+    }
+  }
+
+  useEffect(() => {
+    getEvents();
+  }, []);
 
   return (
     <>
@@ -32,16 +86,45 @@ export default function NewEvents() {
           </span>
 
           {/* h-[calc(100vh-160px)] => Altura do viewport menos o padding do header (Botao de voltar), juntamente com o padding da tela geral */}
-          <div className='w-full flex flex-col gap-5 items-center justify-center leading-tight h-[calc(100vh-160px)]'>
+          <div className='w-full flex flex-col gap-5 items-center leading-tight h-[calc(100vh-160px)]'>
             <p className='text-5xl font-medium mb-4'>Meus Eventos</p>
 
-            <div className='flex flex-col gap-4 overflow-y-auto'>
-              <NewCardsEvent />
-              <NewCardsEvent />
-              <NewCardsEvent />
-              <NewCardsEvent />
-              <NewCardsEvent />
-              <NewCardsEvent />
+            <ModalCreateEvent getEvents={getEvents} />
+
+            <div className='flex flex-col gap-4 overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-100'>
+              {isLoading ? (
+                <div className='flex flex-1 items-center justify-center'>
+                  <ClipLoader color='red' size={40} />
+                </div>
+              ) : (
+                <>
+                  {events.length > 0 ? (
+                    <>
+                      {events.map((eventItem) => (
+                        <>
+                          <NewCardsEvent
+                            key={eventItem.id}
+                            event={eventItem}
+                            onViewDetails={onViewDetails}
+                            onDeleteEvent={onDeleteEvent}
+                          />
+                        </>
+                      ))}
+                    </>
+                  ) : (
+                    <>
+                      <img
+                        src={emptyImage}
+                        alt='List empty events image'
+                        className='w-[200px]'
+                      />
+                      <span className='text-zinc-500 text-base'>
+                        Nenhum evento encontrado
+                      </span>
+                    </>
+                  )}
+                </>
+              )}
             </div>
           </div>
         </div>
