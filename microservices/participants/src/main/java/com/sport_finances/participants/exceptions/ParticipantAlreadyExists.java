d1c2112dd661235c563e4b005926687ade1fbe32
oLucasNascimento <lucasf.ferreira@bandtec.com.br>
2025-05-21 16:28:43 -0300

    chore: included participants-service in project

--- microservices/participants/src/main/java/com/sport_finances/participants/exceptions/ParticipantAlreadyExists.java
+++
@@ -1,7 +0,0 @@
-package com.sport_finances.participants.exceptions;
-
-public class ParticipantAlreadyExists extends RuntimeException {
-    public ParticipantAlreadyExists() {
-        super("Participant already exists in event!");
-    }
-}
--- microservices/participants/src/main/java/com/sport_finances/participants/exceptions/ParticipantNotFound.java
+++
@@ -1,7 +0,0 @@
-package com.sport_finances.participants.exceptions;
-
-public class ParticipantNotFound extends RuntimeException {
-    public ParticipantNotFound() {
-        super("Participant not found!");
-    }
-}
